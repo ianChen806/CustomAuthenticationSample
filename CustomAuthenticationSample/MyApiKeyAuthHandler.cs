112138f32b391ffd49cd73220be94a6a3075b871
IanChen <ian.chen@taiwantaxi.com.tw>
2020-07-03 15:18:35 +0800

    add scheme

---
+++ CustomAuthenticationSample/MyApiKeyAuthHandler.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CustomAuthenticationSample
+{
+    public class MyApiKeyAuthHandler : AuthenticationHandler<MyApiKeyAuthOption>
+    {
+        public MyApiKeyAuthHandler(IOptionsMonitor<MyApiKeyAuthOption> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CustomAuthenticationSample/MyApiKeyAuthOption.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Authentication;
+
+namespace CustomAuthenticationSample
+{
+    public class MyApiKeyAuthOption : AuthenticationSchemeOptions
+    {
+    }
+}
