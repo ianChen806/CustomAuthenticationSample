6caed9cbe3362f61607b6f48ca4eec9254783ea9
IanChen <ian.chen@taiwantaxi.com.tw>
2020-07-03 15:23:21 +0800

    first

--- CustomAuthenticationSample/MyApiKeyAuthHandler.cs
+++ CustomAuthenticationSample/MyApiKeyAuthHandler.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
@@ -9,14 +11,30 @@ namespace CustomAuthenticationSample
 {
     public class MyApiKeyAuthHandler : AuthenticationHandler<MyApiKeyAuthOption>
     {
-        public MyApiKeyAuthHandler(IOptionsMonitor<MyApiKeyAuthOption> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
+        public MyApiKeyAuthHandler(IOptionsMonitor<MyApiKeyAuthOption> options,
+                                   ILoggerFactory logger,
+                                   UrlEncoder encoder,
+                                   ISystemClock clock)
             : base(options, logger, encoder, clock)
         {
         }
 
-        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            throw new NotImplementedException();
+            await Task.CompletedTask;
+
+            var name = string.Empty;
+            var role = string.Empty;
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, name),
+                new Claim(ClaimTypes.Role, role),
+            };
+            var claimsIdentity = new ClaimsIdentity(claims);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            var authenticationTicket = new AuthenticationTicket(claimsPrincipal, "ApiKey");
+            return AuthenticateResult.Success(authenticationTicket);
         }
     }
 }
