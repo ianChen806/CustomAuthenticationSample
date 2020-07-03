0ce965794dc22cb36131fd15c2584bf1b72ee055
IanChen <ian.chen@taiwantaxi.com.tw>
2020-07-03 15:12:42 +0800

    init.

---
+++ CustomAuthenticationSample/Controllers/WeatherForecastController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CustomAuthenticationSample.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "ok";
+        }
+    }
+}
